1d50945693b8ba7549d8ceff81182718d14a00b0
britomarco <marcobritoh@gmail.com>
2018-04-16 14:42:47 -0300

    implementando api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { HttpModule } from "@angular/http";
 
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
 
-import { AppComponent } from './app.component';
-
+import { PokemonListComponent } from "./pokemon/pokemon-list.component";
+import { PokemonDetailComponent } from "./pokemon/pokemon-detail.component";
+import { PokemonService } from "./pokemon/pokemon-service";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
-    BrowserModule
+    AppRoutingModule,
+    BrowserModule,
+    FormsModule,
+    HttpModule,
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    PokemonListComponent,
+    PokemonDetailComponent
+  ],
+  providers: [
+    PokemonService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
